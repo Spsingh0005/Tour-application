eba428092c2042970fd8d718edc69609ca0ffcf0
Robin <robinsingh199815@gmail.com>
2024-04-03 15:26:10 +0530

    port updated

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const app = require("./app");
-const port = 3000;
+const port = process.env.PORT || 3000;
 const dotenv = require("dotenv").config();
 const http = require("http");
 
