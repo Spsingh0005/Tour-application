b9e2dacb1e82c74d3904ed89edfbb1f4aa33a6cd
Robin <robinsingh199815@gmail.com>
2024-04-11 09:58:12 +0530

    final commit port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const app = require("./app");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 require("dotenv").config();
 const http = require("http");
 
